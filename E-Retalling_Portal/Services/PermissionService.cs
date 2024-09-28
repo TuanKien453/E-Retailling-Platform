d0c71c77180bcb3c1d4e3578b70f9bd2a843eac8
Hoang Tuan Kien (K18 HL) <kienhthe182255@fpt.edu.vn>
2024-09-28 02:44:37 +0000

    Feature/category manage

---
+++ E-Retalling_Portal/Services/PermissionService.cs
@@ -0,0 +1,36 @@
+﻿using E_Retalling_Portal.Models;
+
+namespace E_Retalling_Portal.Services
+{
+    //class 
+    public class RolePermissionService
+    {
+        private Dictionary<string, List<Permission>> rolePermissionMappings = new Dictionary<string, List<Permission>>
+    {
+        { "Admin", new List<Permission>
+               {
+                   Permission.ViewCategoryList,
+               }
+        },
+
+    };
+        /// <summary>
+        /// Checks if a role has a specific permission.
+        /// </summary>
+        /// <param name="role">The role to check permission for.</param>
+        /// <param name="permission">The permission to check.</param>
+        /// <returns>True if the role has the specified permission, false otherwise.</returns>
+        public bool HasPermission(string role, Permission permission)
+        {
+            if (role == null)
+            {
+                role = "Guest";
+            }
+            if (rolePermissionMappings.ContainsKey(role))
+            {
+                return rolePermissionMappings[role].Contains(permission);
+            }
+            return false;
+        }
+    }
+}
