f563edd0b7b52aad2e2ada5a43e42e58caffc49d
TuanKien453 <tuankien0408@gmail.com>
2024-09-30 21:28:07 +0700

    delete trash file

--- New folder/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace E_Retalling_Portal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
