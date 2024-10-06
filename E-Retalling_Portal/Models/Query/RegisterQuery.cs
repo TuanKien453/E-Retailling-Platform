0266b329c2dc0fd63c61d653961de58a3e70085a
Phien47 <146061265+Phien47@users.noreply.github.com>
2024-10-06 21:52:10 +0700

    change UI Register, sercurity data

--- E-Retalling_Portal/Models/Query/RegisterQuery.cs
+++ E-Retalling_Portal/Models/Query/RegisterQuery.cs
@@ -4,11 +4,11 @@ namespace E_Retalling_Portal.Models.Query
 {
     public static class RegisterQuery
     {
-        public static IQueryable<User> GetVaildUserData(this DbSet<User> dbUser, string email, string phone)
+        public static IQueryable<User> GetValidUserData(this DbSet<User> dbUser, string email, string phone)
         {
             return dbUser.Where(u => u.email == email || u.phoneNumber == phone);
         }
-        public static IQueryable<Account> GetVaildAccount(this DbSet<Account> dbAccount, string username)
+        public static IQueryable<Account> GetValidAccount(this DbSet<Account> dbAccount, string username)
         {
             return dbAccount.Where(u => u.username == username);
         }
@@ -16,9 +16,28 @@ namespace E_Retalling_Portal.Models.Query
         {
             return dbUser.Where(u => u.email == email);
         }
-        public static IQueryable<Account> GetVaildShopOwnerAccount(this DbSet<Account> dbAccount, string username, int roleId)
+        public static IQueryable<User> GetValidUserData(this DbSet<User> dbUser, string email, string phone, int userId)
+        {
+            return dbUser.Where(u => (u.email == email || u.phoneNumber == phone) && u.id != userId);
+        }
+
+        public static IQueryable<Account> GetValidShopOwnerAccount(this DbSet<Account> dbAccount, string username, int roleId)
         {
             return dbAccount.Where(u => u.username == username && u.roleId == roleId);
+        }
+
+        public static IQueryable<Account> GetValidAccountByUserId(this DbSet<Account> dbAccount, int userId, int roleId)
+        {
+            return dbAccount.Where(u => u.userId == userId && u.roleId == roleId);
+        }
+
+        public static IQueryable<User> GetValidUserDataByEmail(this DbSet<User> dbUser, string email)
+        {
+            return dbUser.Where(u => u.email == email);
+        }
+        public static IQueryable<User> GetValidUserDataByPhone(this DbSet<User> dbUser, string phone)
+        {
+            return dbUser.Where(u => u.phoneNumber == phone);
         }
     }
 }
