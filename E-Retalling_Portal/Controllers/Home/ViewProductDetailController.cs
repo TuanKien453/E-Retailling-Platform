5e2a8b6cf889ed82b2b7ccaeda1462c66bba8d40
anhncdhe181098 <145757300+danhhhm@users.noreply.github.com>
2024-10-16 23:11:13 +0700

    Pagination home page

--- E-Retalling_Portal/Controllers/Home/ViewProductDetailController.cs
+++ E-Retalling_Portal/Controllers/Home/ViewProductDetailController.cs
@@ -10,20 +10,53 @@ namespace E_Retalling_Portal.Controllers.Home
         {
             using (var context = new Context())
             {
-                int? accountId = HttpContext.Session.GetInt32(SessionKeys.AccountId.ToString());
                 var product = context.Products.GetProductById(productId.Value).FirstOrDefault();
                 var productItemList = context.ProductItems.GetProductItem(productId.Value).ToList();
-                var similarProducts = context.Products.GetSimilarProductByProductCategory(product.category,product.id).ToList();
-                double minPrice = productItemList.Min(pi => pi.price);
-                double maxPrice = productItemList.Max(pi => pi.price);
-
-                ViewBag.minPrice = minPrice; ViewBag.maxPrice = maxPrice;
-                ViewBag.accountId = accountId;
+                var similarProducts = context.Products.GetSimilarProductByProductCategory(product.category, product.id).ToList();
+                if (product.isVariation == true)
+                {
+                    double minPrice = productItemList.Min(pi => pi.price);
+                    double maxPrice = productItemList.Max(pi => pi.price);
+                    ViewBag.minPrice = minPrice; ViewBag.maxPrice = maxPrice;
+                }
+                List<Product> products = GetProductsIsNotDelete(similarProducts);
+                ViewBag.productImageList = product.images;
                 ViewBag.product = product;
+                Console.WriteLine(product.quantity+","+product.price);
                 ViewBag.productItemList = productItemList;
-                ViewBag.similarProducts = similarProducts;
+                ViewBag.similarProducts = products;
             }
             return View("/Views/Home/ViewProductDetail.cshtml");
+        }
+        private List<Product> GetProductsIsNotDelete(List<Product>? productList)
+        {
+            List<Product> products = new List<Product>();
+            int count = 0;
+            foreach (var product in productList)
+            {
+                if (count >= 10)
+                {
+                    break;
+                }
+                if (product.isVariation == true)
+                {
+                    foreach (var productItem in product.productItems)
+                    {
+                        if (productItem.deleteAt == null)
+                        {
+                            products.Add(product);
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    products.Add(product);
+                    count++;
+                }
+            }
+            return products;
         }
     }
 }
--- E-Retalling_Portal/Models/Query/ProductQuery.cs
+++ E-Retalling_Portal/Models/Query/ProductQuery.cs
@@ -14,19 +14,11 @@ namespace E_Retalling_Portal.Models.Query
         }
         public static IQueryable<Product> GetProduct(this DbSet<Product> dbProduct)
         {
-            return dbProduct
-        .Include(p => p.coverImage)
-        .Include(p => p.images)
-        .Include(p => p.productItems)  // Load danh sách productItems
-        .Where(p => p.deleteAt == null
-                    && p.isVariation == true
-                    && p.productItems != null
-                    && p.productItems.Count > 0);
+            return dbProduct.Include("coverImage").Include("images").Include("productItems").Where(p => p.deleteAt == null || (p.productItems.Count > 0 && p.isVariation == true) );
         }
         public static IQueryable<Product> GetSimilarProductByProductCategory(this DbSet<Product> dbProduct, Category category, int productId)
         {
-            return dbProduct.Include(p => p.coverImage).Include(p => p.images).Include(p => p.productItems).Include(p => p.category).Where(p => p.deleteAt == null && p.category.parentCategoryId == category.parentCategoryId && p.productItems != null && p.id != productId
-                    && p.productItems.Count > 0);
+            return dbProduct.Include(p => p.coverImage).Include(p => p.images).Include(p => p.productItems).Include(p => p.category).Where(p => p.deleteAt == null && p.category.parentCategoryId == category.parentCategoryId && p.id != productId || (p.productItems.Count > 0 && p.id != productId));
         }
 
 
