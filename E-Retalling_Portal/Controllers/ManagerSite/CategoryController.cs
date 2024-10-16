e5d859951f31a26204f363bfa0e1927c22150c92
quynxhe186459 <quya1k48@gmail.com>
2024-10-16 23:22:29 +0700

    To page list

--- E-Retalling_Portal/Controllers/ManagerSite/CategoryController.cs
+++ E-Retalling_Portal/Controllers/ManagerSite/CategoryController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace E_Retalling_Portal.Controllers.Manager
 {
