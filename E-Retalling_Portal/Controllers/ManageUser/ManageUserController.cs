cc5e889d82b9e32f0c0f5e5c9d9324c23c15ff04
Trang05012003 <trangnthhe170220@fpt.edu.vn>
2024-10-16 22:38:40 +0700

    ManageShop, ManageUser

---
+++ E-Retalling_Portal/Controllers/ManageUser/ManageUserController.cs
@@ -0,0 +1,21 @@
+﻿using E_Retalling_Portal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Retalling_Portal.Controllers.ManageUser
+{
+    public class ManageUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var context = new Context())
+            {
+                var userList = context.Users.ToList();
+                ViewBag.Users = userList;
+             
+               
+            }
+
+            return View("/Views/ManageUser/ManageUser.cshtml");
+        }
+    }
+}
---
+++ E-Retalling_Portal/Controllers/ShopManage/ShopManageController.cs
@@ -0,0 +1,20 @@
+﻿using E_Retalling_Portal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Retalling_Portal.Controllers.ShopManager
+{
+	public class ShopManageController : Controller
+	{
+		public IActionResult Index()
+		{
+            using (var context = new Context())
+            {
+				var shopList= context.Shops.ToList();
+				ViewBag.Shops= shopList;
+				
+			}
+
+			return View("/Views/ManageShop/ManageShop.cshtml");
+		}
+	}
+}
