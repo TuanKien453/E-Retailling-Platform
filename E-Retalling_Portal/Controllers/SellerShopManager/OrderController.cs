70260951f22b9ebadae6e1c83b47c5945c1d9456
anhncdhe181098 <145757300+danhhhm@users.noreply.github.com>
2024-11-08 22:38:46 +0700

    update shipping

--- E-Retalling_Portal/Controllers/SellerShopManager/OrderController.cs
+++ E-Retalling_Portal/Controllers/SellerShopManager/OrderController.cs
@@ -12,64 +12,71 @@ using X.PagedList.Extensions;
 
 namespace E_Retalling_Portal.Controllers.SellerShopManager
 {
-	public class OrderController : Controller
-	{
-		private readonly GHNService _ghnService;
-		public OrderController(GHNService ghnService)
-		{
-			_ghnService = ghnService;
-		}
-		public async Task<IActionResult> OrderInfoAsync()
-		{
-			using (var context = new Context())
-			{
+    public class OrderController : Controller
+    {
+        private readonly GHNService _ghnService;
+        public OrderController(GHNService ghnService)
+        {
+            _ghnService = ghnService;
+        }
+        public async Task<IActionResult> OrderInfoAsync()
+        {
+            using (var context = new Context())
+            {
                 int? accountId = (int)HttpContext.Session.GetInt32(SessionKeys.AccountId.ToString());
                 Account account = context.Accounts.GetAccountByAccountId(accountId.Value).FirstOrDefault();
-				var shop = context.Shops.GetShopbyAccId(accountId.Value).FirstOrDefault();
+                var shop = context.Shops.GetShopbyAccId(accountId.Value).FirstOrDefault();
                 var orderItemList = context.OrderItems.GetOrderItemByShopId(shop.id).ToList();
-				var orderInfoResponses = new List<OrderInfoResponse>();
-				foreach (var orderItem in orderItemList)
-				{
-					if (orderItem.externalOrderCode != null)
-					{
-						var orderInfoResponse = await _ghnService.GetOrderInfoAsync(orderItem.externalOrderCode);
-							orderInfoResponses.Add(orderInfoResponse);
-						orderItem.createAt = orderInfoResponse.Data.CreatedDate;
-						orderItem.finishAt = orderInfoResponse.Data.FinishDate;
-						orderItem.shippingStatus = orderInfoResponse.Data.Status;
-                        context.Update(orderItem);
-                    }
-                }
-				var joinedData = GetDataFromOrsedItemAndOrderInfoResponse(orderItemList, orderInfoResponses);
-				foreach(var item in joinedData)
-				{
-                    Console.WriteLine(item.ShippingStatus);
-				}
-				ViewBag.OrderInfo = joinedData;
-			}
-			return View("/Views/SellerShopManager/Order/OrderInfo.cshtml");
-		}
-		public async Task<IActionResult> ViewOrderList(int? page, int pageSize = 1, string status = "all")
-		{
-			using (var context = new Context())
-			{
-				int? accountId = (int)HttpContext.Session.GetInt32(SessionKeys.AccountId.ToString());
-				Account account = context.Accounts.GetAccountByAccountId(accountId.Value).FirstOrDefault();
-				User user = context.Users.GetUserByUserIdInAccount(account.userId).FirstOrDefault();
-				var orderItemList = context.OrderItems.GetOrderItemByUserId(user.id).ToList();
-				var orderInfoResponses = new List<OrderInfoResponse>();
-				foreach (var orderItem in orderItemList)
-				{
-					if (orderItem.externalOrderCode != null)
-					{
-						var orderInfoResponse = await _ghnService.GetOrderInfoAsync(orderItem.externalOrderCode);
-                            orderInfoResponses.Add(orderInfoResponse);
-                        orderItem.createAt = orderInfoResponse.Data.CreatedDate;
-                        orderItem.finishAt = orderInfoResponse.Data.FinishDate;
+                var orderInfoResponses = new List<OrderInfoResponse>();
+                foreach (var orderItem in orderItemList)
+                {
+                    if (orderItem.externalOrderCode != null)
+                    {
+                        var orderInfoResponse = await _ghnService.GetOrderInfoAsync(orderItem.externalOrderCode);
+                        orderInfoResponses.Add(orderInfoResponse);
+                        string formattedDate = DateTime.TryParse(orderInfoResponse.Data.FinishDate, out DateTime parsedDate)
+                        ? parsedDate.ToString("yyyyMMddHHmmss")
+                        : "";
+                        orderItem.order.endTime = formattedDate;
                         orderItem.shippingStatus = orderInfoResponse.Data.Status;
                         context.Update(orderItem);
+                        context.SaveChanges();
                     }
-				}
+                }
+                var joinedData = GetDataFromOrsedItemAndOrderInfoResponse(orderItemList, orderInfoResponses);
+                foreach (var item in joinedData)
+                {
+                    Console.WriteLine(item.ShippingStatus);
+                }
+                ViewBag.OrderInfo = joinedData;
+            }
+            return View("/Views/SellerShopManager/Order/OrderInfo.cshtml");
+        }
+        public async Task<IActionResult> ViewOrderList(int? page, int pageSize = 1, string status = "all")
+        {
+            using (var context = new Context())
+            {
+                int? accountId = (int)HttpContext.Session.GetInt32(SessionKeys.AccountId.ToString());
+                Account account = context.Accounts.GetAccountByAccountId(accountId.Value).FirstOrDefault();
+                User user = context.Users.GetUserByUserIdInAccount(account.userId).FirstOrDefault();
+                var orderItemList = context.OrderItems.GetOrderItemByUserId(user.id).ToList();
+                var orderInfoResponses = new List<OrderInfoResponse>();
+                foreach (var orderItem in orderItemList)
+                {
+                    if (orderItem.externalOrderCode != null)
+                    {
+                        var orderInfoResponse = await _ghnService.GetOrderInfoAsync(orderItem.externalOrderCode);
+                        orderInfoResponses.Add(orderInfoResponse);
+                        string formattedDate = DateTime.TryParse(orderInfoResponse.Data.FinishDate, out DateTime parsedDate)
+                            ? parsedDate.ToString("yyyyMMddHHmmss")
+                            : "";
+                        orderItem.order.endTime = formattedDate;
+                        orderItem.shippingStatus = orderInfoResponse.Data.Status;
+
+                        context.Update(orderItem);
+                        context.SaveChanges();
+                    }
+                }
                 var joinedData = GetDataFromOrsedItemAndOrderInfoResponse(orderItemList, orderInfoResponses);
                 if (!string.IsNullOrEmpty(status) && status != "all")
                 {
@@ -84,8 +91,8 @@ namespace E_Retalling_Portal.Controllers.SellerShopManager
 
 
                 return View("/Views/SellerShopManager/Order/ViewOrderList.cshtml");
-			}
-		}
+            }
+        }
         private IEnumerable<dynamic> GetDataFromOrsedItemAndOrderInfoResponse(List<OrderItem> orderItemList, List<OrderInfoResponse> orderInfoResponses)
         {
             var joinedData = orderInfoResponses
@@ -110,14 +117,21 @@ namespace E_Retalling_Portal.Controllers.SellerShopManager
                                 Status = orderInfo.Data.Status,
                                 Weight = orderInfo.Data.Weight,
                                 Price = orderItem.price,
-								Product = orderItem.product,
-								ProductItem = orderItem.productItem,
-								ProductItemId = orderItem.productItemId,
-								OrderItem = orderItem,
-								CreateDate = orderInfo.Data.CreatedDate,
-								FinishDate = orderInfo.Data.FinishDate
+                                Product = orderItem.product,
+                                ProductItem = orderItem.productItem,
+                                ProductItemId = orderItem.productItemId,
+                                OrderItem = orderItem,
+                                CreateDate = formatDate(orderInfo.Data.CreatedDate),
+                                FinishDate = formatDate(orderInfo.Data.FinishDate)
                             });
             return joinedData;
+        }
+        private string formatDate( string date)
+        {
+             string formattedDate = DateTime.TryParse(date, out DateTime parsedDate)
+                            ? parsedDate.ToString("yyyy-MM-dd  HH:mm")
+                            : "";
+            return formattedDate;
         }
 
     }
